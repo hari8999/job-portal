37b5018be4f5d2a5639a9d1a525d225adb1389c4
HARIHARAN DNYANESHWAR PARMAR <90195086+hari8999@users.noreply.github.com>
2022-04-06 07:47:37 +0530

    Add files via upload

--- job-port/backend/index.js
+++ job-port/backend/index.js
@@ -1,1 +1,32 @@
 
+import express from 'express';
+import dotenv from "dotenv";
+
+//component
+import Connection from './database/db.js';
+ import Routes from './routes/Routes.js';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json({extended:true}));
+app.use(bodyParser.urlencoded({extended:true}));
+ app.use('/', Routes);
+ 
+const PORT1 = 8000; 
+
+app.get('/',(req,res)=>{
+    res.json("server start")
+})
+
+const username= process.env.MONGO_USERNAME;
+const password= process.env.MONGO_PASSWORD;
+
+Connection(username,password);
+app.listen(process.env.PORT ||PORT1, console.log(`Server is listening on PORT no. ${PORT1}`));
+
+
+
+
