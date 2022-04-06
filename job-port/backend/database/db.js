161181c1f00aaad8aca179d01069d6c09e9d9f16
HARIHARAN DNYANESHWAR PARMAR <90195086+hari8999@users.noreply.github.com>
2022-04-06 07:42:51 +0530

    Add files via upload

--- job-port/backend/database/db.js
+++ job-port/backend/database/db.js
@@ -1,1 +1,15 @@
+import mongoose from 'mongoose';
 
+const Connection = async (username,password) =>{
+    
+    const URL=`mongodb+srv://${username}:${password}@cluster0.8a4n4.mongodb.net/JOB_PORTAL?retryWrites=true&w=majority`;
+    try{
+        await mongoose.connect(URL,{useUnifiedTopology:true, useNewUrlParser:true});
+        console.log("You have successfully connected to MongoDB database");
+    }
+    catch(error){
+        console.log("Error while connecting to MongoDB",error);
+    }
+}
+
+export default Connection;
