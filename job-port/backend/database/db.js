8ae34a9eaacc39adc3df3f7f650da440036b4c18
HARIHARAN DNYANESHWAR PARMAR <90195086+hari8999@users.noreply.github.com>
2022-04-06 15:08:40 +0530

    Update db.js

--- job-port/backend/database/db.js
+++ job-port/backend/database/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const Connection = async (username,password) =>{
     
-    const URL=`mongodb+srv://${username}:${password}@cluster0.8a4n4.mongodb.net/JOB_PORTAL?retryWrites=true&w=majority`;
+    const URL=`mongodb+srv://${username}:${password}@cluster0.8a4n4.mongodb.net/JOB-PORTAL?retryWrites=true&w=majority`;
     try{
         await mongoose.connect(URL,{useUnifiedTopology:true, useNewUrlParser:true});
         console.log("You have successfully connected to MongoDB database");
